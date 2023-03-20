d2509b3fe0ec84da4b1e95b4ffe0d506d5bfe150
Jack Lingle <captainjack0818@gmail.com>
2023-03-20 11:57:17 -0400

    Add error borders and confirm borders + flex -end alignment to form inputs

--- validate.js
+++ validate.js
@@ -5,8 +5,17 @@ function validatePassword(e) {
     let pwd = document.getElementById('pwd').value;
     let confirmation = document.getElementById('confirm-pwd').value;
     if(pwd === confirmation) {
-        
+        // remove error
+        document.getElementById('pwd').classList.remove('error');
+        document.getElementById('confirm-pwd').classList.remove('error');
+        // add green for confirm
+        document.getElementById('pwd').classList.add('confirm');
+        document.getElementById('confirm-pwd').classList.add('confirm');
     } else {
+        // add error
+        document.getElementById('pwd').classList.add('error');
+        document.getElementById('confirm-pwd').classList.add('error');
+        // prevents form from being submitted
         e.preventDefault();
     }
 }
